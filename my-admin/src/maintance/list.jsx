237b9bb794a23d4fadcbc3f342785e29b4338095
Fl0renceP <141628724+Fl0renceP@users.noreply.github.com>
2025-11-28 10:40:14 +0000

    linking data provider

---
+++ my-admin/src/maintance/list.jsx
@@ -0,0 +1,30 @@
+import { DataTable, DateField, EmailField, List } from 'react-admin';
+
+export const Maintenance_recordList = () => (
+    <List>
+        <DataTable>
+            <DataTable.Col source="address" />
+            <DataTable.Col source="created_at">
+                <DateField source="created_at" />
+            </DataTable.Col>
+            <DataTable.Col source="customer_number" />
+            <DataTable.Col source="date_of_birth">
+                <DateField source="date_of_birth" />
+            </DataTable.Col>
+            <DataTable.Col source="drivers_license" />
+            <DataTable.Col source="email">
+                <EmailField source="email" />
+            </DataTable.Col>
+            <DataTable.Col source="id" />
+            <DataTable.NumberCol source="is_blacklisted" />
+            <DataTable.Col source="license_expiry">
+                <DateField source="license_expiry" />
+            </DataTable.Col>
+            <DataTable.Col source="name" />
+            <DataTable.Col source="notes" />
+            <DataTable.Col source="phone" />
+            <DataTable.Col source="registration_date">
+                <DateField source="registration_date" />
+            </DataTable.Col>
+        </DataTable>
+    </List>)
